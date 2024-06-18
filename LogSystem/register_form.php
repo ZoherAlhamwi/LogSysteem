c286fe66538fdcea37ab356e8fefe4f4e495a401
ZoherAlhamwi <115456139+ZoherAlhamwi@users.noreply.github.com>
2024-06-18 13:09:25 +0200

    LogSysteem
    Logsysteem is een efficiënte website, ontwikkeld met HTML, CSS, PHP, en MySQL, ontworpen om soepel samen te werken met MySQL-databases.

---
+++ LogSystem/register_form.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register form</title>
+
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>register now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+
+      };
+      ?>
+      <input type="text" name="name" required placeholder="Enter your name">
+      <input type="email" name="email" required placeholder="Enter your email">
+      <input type="password" name="password" required placeholder="Enter your password">
+      <input type="password" name="cpassword" required placeholder="Confirm your password">
+      <select name="user_type">
+         <option value="user">User</option>
+         <option value="admin">Admin</option>
+      </select>
+      <input type="submit" name="submit" value="register now" class="form-btn">
+      <p>already have an account? <a href="login_form.php">login now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
